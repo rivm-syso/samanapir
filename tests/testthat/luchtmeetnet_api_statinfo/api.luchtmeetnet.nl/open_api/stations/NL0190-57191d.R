d7a7b9da3c8d6920c39dfce071bc64b694e5b7ad
Elma Tenner <tennere@rivm-comp-l19p.rivm.ssc-campus.nl>
2026-07-17 13:50:36 +0200

    Data for the tests luchtmeetnet api #10

---
+++ tests/testthat/luchtmeetnet_api_statinfo/api.luchtmeetnet.nl/open_api/stations/NL0190-57191d.R
@@ -0,0 +1,14 @@
+structure(list(method = "GET", url = "https://api.luchtmeetnet.nl/open_api/stations/NL0190/?page=1", 
+    status_code = 404L, headers = structure(list(`Content-Type` = "application/json", 
+        `Content-Length` = "47", Connection = "keep-alive", Date = "Fri, 17 Jul 2026 07:03:02 GMT", 
+        `x-amzn-Remapped-Date` = "Fri, 17 Jul 2026 07:03:02 GMT", 
+        `x-amzn-RequestId` = "4d014d10-d6ad-4b4d-897c-52603c6cead9", 
+        `Access-Control-Allow-Origin` = "*", `x-amzn-Remapped-Content-Length` = "47", 
+        `x-amzn-Remapped-Connection` = "keep-alive", `x-amz-apigw-id` = "Ao4B_FviFiAEThg=", 
+        `x-amzn-Remapped-Server` = "nginx", `X-Cache` = "Error from cloudfront", 
+        Via = "1.1 6553b2bbd8fca4153c739e94065a1184.cloudfront.net (CloudFront)", 
+        `X-Amz-Cf-Pop` = "AMS1-P3", `X-Amz-Cf-Id` = "dvkPVpDk_w8nDM46SPj0-z-wRL916Eb24AkpW7hzzlR_5miIvtBuhg=="), class = "httr2_headers"), 
+    body = charToRaw("{\"reason\": \"not_found\", \"message\": \"Not Found\"}"), 
+    timing = c(redirect = 0, namelookup = 2.4e-05, connect = 2.4e-05, 
+    pretransfer = 6.8e-05, starttransfer = 0.575492, total = 0.57553
+    ), cache = new.env(parent = emptyenv())), class = "httr2_response")
