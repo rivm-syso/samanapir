b5bbb3aab72068334cf8c1118b62ff79fc1dfd93
Elma Tenner <tennere@rivm-comp-l23p.rivm.ssc-campus.nl>
2026-07-20 17:00:34 +0200

    For the pagination special one for samen meten, functions for get obs#10

--- R/GetDataPagination.R
+++ R/GetDataPagination.R
@@ -39,7 +39,7 @@ GetDataPagination <- function(content, url_page){
 
     # Check if api call is succesful
     if(is.null(content_next)){
-      logger::log_info(paste0("Not succesful: ", url_next))
+      logger::log_info(paste0("Not succesful: ", url_next$url))
       return(data_totaal)
     }
 
@@ -56,6 +56,68 @@ GetDataPagination <- function(content, url_page){
   # logging
   logger::log_debug(paste0("All pages done:  ", current_page, " of ",
                            last_page))
+
+  return(data_totaal)
+}
+
+#' GetDataPaginationSamenMeten
+#'
+#' Get the data from the API from all the pages, see GetAPIDataframe
+#' for the details from getting the data
+#'
+#' @param content resp of first data request using GetAPIDataframe
+#'
+#' @returns list with all the data from the different pages, depending on return api
+GetDataPaginationSamenMeten <- function(content){
+  data_totaal <- content$value
+
+  # Check if there is a new page
+  if("@iot.nextLink" %in% names(content)){
+    check_next <- TRUE
+    }else{
+      check_next <- FALSE
+      return(content$value)
+    }
+
+  # Haalt de volgende pagina's op
+  while(check_next){
+    # Get the url string for the next page
+    next_page <- content$`@iot.nextLink`
+
+    # Maak de url request voor de volgende pagina en haal data op
+    url_next <- httr2::request(next_page)
+
+    # logging debug
+    logger::log_debug(paste0("Data ophalen van: ", url_next$url))
+
+    # Get the data from API
+    content_next <- GetAPIDataframe2(url_next)
+
+    # Check if api call is succesful
+    if(is.null(content_next)){
+      logger::log_info(paste0("Not succesful: ", url_next$url))
+      return(data_totaal)
+    }
+    # Get the data of the new page
+    data_new <- content_next$value
+
+    # Add the data to the total
+    data_totaal <- append(data_totaal, data_new)
+
+    # Check of er nog een nieuwe pagina is
+    if("@iot.nextLink" %in% names(content_next)){
+      check_next <- TRUE
+    }else{
+      # logging
+      logger::log_debug(paste0("All pages done"))
+
+      check_next <- FALSE
+      return(data_totaal)
+    }
+  }
+
+  # logging
+  logger::log_debug(paste0("All pages done"))
 
   return(data_totaal)
 }
