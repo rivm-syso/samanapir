a65a4e72ae77057354756efa5a8c643f253397e7
Elma Tenner <tennere@rivm-comp-l18p.rivm.ssc-campus.nl>
2026-09-09 15:44:44 +0200

    test files from the apiss mocked #16, #10

--- tests/testthat/luchtmeetnet_api_statinfo/api.luchtmeetnet.nl/open_api/stations/NL0190-57191d.R
+++ tests/testthat/luchtmeetnet_api_statinfo/api.luchtmeetnet.nl/open_api/stations/NL0190-57191d.R
@@ -1,14 +1,14 @@
 structure(list(method = "GET", url = "https://api.luchtmeetnet.nl/open_api/stations/NL0190/?page=1", 
     status_code = 404L, headers = structure(list(`Content-Type` = "application/json", 
-        `Content-Length` = "47", Connection = "keep-alive", Date = "Fri, 17 Jul 2026 07:03:02 GMT", 
-        `x-amzn-Remapped-Date` = "Fri, 17 Jul 2026 07:03:02 GMT", 
-        `x-amzn-RequestId` = "4d014d10-d6ad-4b4d-897c-52603c6cead9", 
+        `Content-Length` = "47", Connection = "keep-alive", Date = "Wed, 09 Sep 2026 10:30:42 GMT", 
+        `x-amzn-Remapped-Date` = "Wed, 09 Sep 2026 10:30:42 GMT", 
+        `x-amzn-RequestId` = "1be8826b-a4fa-404b-a1a5-71f17170f1b7", 
         `Access-Control-Allow-Origin` = "*", `x-amzn-Remapped-Content-Length` = "47", 
-        `x-amzn-Remapped-Connection` = "keep-alive", `x-amz-apigw-id` = "Ao4B_FviFiAEThg=", 
+        `x-amzn-Remapped-Connection` = "keep-alive", `x-amz-apigw-id` = "DbVE6H_iFiAEMIQ=", 
         `x-amzn-Remapped-Server` = "nginx", `X-Cache` = "Error from cloudfront", 
-        Via = "1.1 6553b2bbd8fca4153c739e94065a1184.cloudfront.net (CloudFront)", 
-        `X-Amz-Cf-Pop` = "AMS1-P3", `X-Amz-Cf-Id` = "dvkPVpDk_w8nDM46SPj0-z-wRL916Eb24AkpW7hzzlR_5miIvtBuhg=="), class = "httr2_headers"), 
+        Via = "1.1 416dae0837568c2bb7cea7ae5c6bba22.cloudfront.net (CloudFront)", 
+        `X-Amz-Cf-Pop` = "AMS58-P5", `X-Amz-Cf-Id` = "qKnbaHaeXN1pXMKZxPKiM8eHBYcRLHRp6i5KG36TODc-F-Wg7d8clw=="), class = "httr2_headers"), 
     body = charToRaw("{\"reason\": \"not_found\", \"message\": \"Not Found\"}"), 
-    timing = c(redirect = 0, namelookup = 2.4e-05, connect = 2.4e-05, 
-    pretransfer = 6.8e-05, starttransfer = 0.575492, total = 0.57553
+    timing = c(redirect = 0, namelookup = 2.6e-05, connect = 2.6e-05, 
+    pretransfer = 7e-05, starttransfer = 0.096439, total = 0.09646
     ), cache = new.env(parent = emptyenv())), class = "httr2_response")
